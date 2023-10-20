ebc822c1b22da9e20daeb896bfbefafa4547b8be
Dadacea Alex <dadaceaalex@gmail.com>
2023-10-20 11:48:47 +0300

    ok

--- ex3.py
+++ ex3.py
@@ -1,9 +1,1 @@
-i = 15 
-j = 22
-val1 = i and j
-print(val1)
-val2 = i & j
-print(val2)
-
-git config  user.email "dadaceaalex@gmail.com"
-  git config user.name "Dadacea Alex"
+print(0.0000000000000000000001)
